306c766e7ddd17e41aaa2d3c674a518f5bdac6af
mantonoiu <antonoiumadalin@gmail.com>
2021-03-16 12:51:17 +0000

    Campaign Card.Group and Create Campaign Button

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import React from "react"
 import factory from '../ethereum/factory';
 
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Button } from "semantic-ui-react";
+
+
 class CampaignIndex extends React.Component {
     static async getInitialProps() { //with static we skip component rendering
         const campaigns = await factory.methods.getAllDeployedCampaigns().call();
         return { campaigns }
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        })
+
+        return <Card.Group items={items} />
+    }
+
     render() {
         return <div>
-            <h1>Hi</h1>
-            <h1>{this.props.campaigns[0]}</h1>
-            {/* <h1>{this.props.campaigns[0]}</h1>  */}
+            <div>{this.renderCampaigns()}</div>
+            <Button content='Create Campaign' icon='add circle' primary
+            />
         </div>
     }
 }
